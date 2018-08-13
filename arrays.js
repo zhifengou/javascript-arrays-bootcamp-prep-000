9642c8eef3a38dd925849f761ccb06919c17a024
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-13 17:39:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,6 @@ function destructivelyAddElementToBeginningOfArray(array, element)
   array.unshift(element)
   return array
 }
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
