e95393a3c73650fdecf9f80bfe04a20daeaf3809
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-13 17:36:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars=["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return [element,array]
 }
