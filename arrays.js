53e28ec9d7fb91381bb1a6e275511d094b6aa9fd
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-13 17:53:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,8 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop()
+  array.pop()
+  return array
 }
 function accessElementInArray(array, index) {
   return array[index]
